aafc7b446788ffa596b9cb83e7bf6a9050184836
Kyle Grier <grier@kth.se>
2017-10-20 21:47:02 -0500

    Added midterm to train on AWS

---
+++ Midterm/preprocess.py
@@ -0,0 +1,55 @@
+import numpy as np
+import pandas as pd
+from scipy.stats import skew
+from sklearn.preprocessing import StandardScaler
+from collections import Counter
+
+# Fill the NaN locations for the columns given with the
+# replacement values given
+def fixNaN(df, cols, replace):
+	fixer = zip(cols, replace)
+	for col, rep in fixer :
+		df[col] = df[col].fillna(rep)
+	return df
+
+# Remove columns in df based on the labels in to_remove
+def removeCols(df):
+	to_remove = ['F19']
+	df = df.drop(to_remove, 1)
+	return df
+
+# Get the features and labels for the training set
+def getXy(df):
+	y = df['Y']
+	X = df.loc[:,'F1':'F27']
+	return X, y
+
+# Show all features that are largely the same value
+def showCounts(X):
+	feat_percent = {}
+	len_feat = len(X)
+	for col in X.columns:
+		feat_count = Counter(X[col].values)
+		largest = float(feat_count.most_common(1)[0][1])
+		feat_percent[col] = largest/len_feat
+	return feat_percent
+
+# Do preprocessing for the training set
+def preprocess_train():
+	train = pd.read_csv("train_final.csv")
+	X, y = getXy(train)
+
+	X = removeCols(X)
+
+	# cols and replace have to be same length
+	cols = ['F5']
+	replace = [0.0]
+	X = fixNaN(X, cols, replace)
+	return X, y
+
+
+# Do preprocessing for the test set
+def preprocess_test():
+	return
+
+#if __name__ == "__main__":
---
+++ Midterm/train.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import numpy as np
+import xgboost as xgb
+import time
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split, StratifiedKFold
+from preprocess import preprocess_train
+from sklearn.model_selection import GridSearchCV
+
+#Get base accuracy
+#all_zero = np.zeros(train['id'].count())
+#base = np.absolute(y - all_zero)
+
+X, y = preprocess_train()
+#import the training set
+
+# splits for validation
+kf = StratifiedKFold(n_splits=5, random_state=1)
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.3,stratify=y)
+
+cv_params = {'max_depth': [3,5,7], 'min_child_weight': [1,3,5]}
+#cv_params = {'learning_rate': [0.1, 0.01], 'subsample': [0.7,0.8,0.9]}
+ind_params = {'n_estimators': 1000, 'seed':0, 'colsample_bytree': 0.8, 
+             'objective': 'binary:logistic'}
+
+opt_xgb = GridSearchCV(xgb.XGBClassifier(**ind_params), 
+                            cv_params, 
+                             scoring = 'accuracy', cv = kf, n_jobs= -1) 
+
+opt_xgb.fit(X, y)
+print(opt_xgb.grid_scores_)
+#XGBoost
+'''
+for train_idx, test_idx in kf.split(X,y):
+    dtrain = xgb.DMatrix()
+    dtest = xgb.DMatrix('demo/data/agaricus.txt.test')
+    model = xgb.XGBClassifier(n_estimators=100, max_depth=9) 
+    model.fit(X.iloc[train_idx], y.iloc[train_idx])
+    y_pred = model.predict(X.iloc[test_idx])
+    print(accuracy_score(y.iloc[test_idx], y_pred))
+'''
